1a74309b22e9ca1366c29ee551f800c0f4291c92
GaetanoMondelli <gaetano.mondelli@gmail.com>
2018-06-28 16:48:16 -0700

    raw events conversion to battle events

--- battle_event_generator.py
+++ battle_event_generator.py
@@ -1,4 +1,6 @@
-trainer = 'gaetano'
+trainer_name = 'gaetano'
+opponent_name = 'opponent'
+manager_name = 'game'
 
 with open('poke.log') as f:
     events = f.readlines()
@@ -7,21 +9,37 @@ events = [(x[:-1]).split(',') for x in events]
 
 my_pokemon = {}
 opponent = {}
-move = -1
+manager = {}
 
-snapshot_sender {}
-snapshot_receiver = {}
+battle_active = False
+
+def bind_event(event):
+    if event[0] == trainer_name:
+        my_pokemon[event[1]] = event[3]
+    if event[0] == opponent_name:
+        opponent[event[1]] = event[3]
+    if event[0] == manager_name:
+        manager[event[1]] = event[3]
+
+def print_pokemon(pokemon):
+    pass
 
 for event in events:
-    if event[0] == trainer:
-        my_pokemon[event[1]] = event[3]
-    if event[0] == trainer:
-        opponent[event[1]] = event[3]
-    if event[0] == trainer and event[1] == 'move':
-        snapshot_sender = my_pokemon
-    else:
-        snapshot_sender = opponent
-            
+    bind_event(event)
+    battle_active = ('battle' in manager and int(manager['battle']) > 0)
+    
+    #if battle_active and event[1] == 'turn':
+    #   print(event[3])
+    #   print('my ',my_pokemon)
+    #   print('op ',opponent)
+
+    if battle_active and event[1] == 'move' and int(manager['turn']) > 0:
+        #print(event[3])
+        print('my ',my_pokemon)
+        print('op ',opponent)
+        print (event[0] +' uses'+ event[3])
+        print('----------------------')
+
 
 
 
